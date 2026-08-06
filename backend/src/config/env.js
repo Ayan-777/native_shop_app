554ee746aa4967b50d079a247a7ebcaca1d63516
Ayan-777 <mondalayan01876@gmail.com>
2026-08-06 16:04:22 +0530

    testing

---
+++ backend/src/config/env.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ENV = {
+    NODE_ENV:process.env.NODE_ENV,
+    PORT:process.env.PORT,
+    DB_URL:process.env.DB_URL
+};
+
+export default ENV;
---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import ENV from "./config/env.js";
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get('/api/check',(req, res) => {
+    res.status(200).json({message : "Success"})
+})
+
+// app ready for deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../admin/dist")));
+    
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+    });
+}
+
+app.listen(ENV.PORT, () => {
+    console.log(`Sarver is runing on ${"http://localhost:8000"} .....`)
+})
