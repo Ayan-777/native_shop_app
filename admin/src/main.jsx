b13f6f520518babe0a279d578070a659c9943bdd
Ayan-777 <mondalayan01876@gmail.com>
2026-08-11 19:31:19 +0530

    auth implemented

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/react'
 import './index.css'
 import App from './App.jsx'
 
+// Import Publishable Key from Vite env
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,70 @@
+import { PhoneNumber } from "@clerk/express";
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  label: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  pinCoode: {
+    type: String,
+    required: true,
+  },
+  PhoneNumber: {
+    type: String,
+    required: true,
+  },
+  isDefault: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    addresses: [addressSchema],
+    wishlist: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
