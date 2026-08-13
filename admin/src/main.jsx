f148f965169da19f7e62c8354f2cc771c0d31ac3
Ayan-777 <mondalayan01876@gmail.com>
2026-08-13 23:17:47 +0530

    fix/inngest-typo

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -11,10 +11,9 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
     </ClerkProvider>
   </StrictMode>,
